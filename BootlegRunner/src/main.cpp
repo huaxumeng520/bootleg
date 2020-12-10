4ba22ff658522aff88101e7fbd658e881dbd3421
haikarainen <fredrik.haikarainen@gmail.com>
2020-12-10 15:12:21 +0100

    Posix/BSD signal handling

--- BootlegRunner/src/main.cpp
+++ BootlegRunner/src/main.cpp
@@ -1,10 +1,35 @@
 
 #include <KIT/Engine.hpp>
 
+#include <csignal>
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+
+
+namespace 
+{
+	kit::Engine* engine = nullptr;
+
+	void handleInterrupt(int s)
+	{
+		engine->shutdown();
+	}
+
+}
+
 int main(int argc, char**argv)
 {
-	kit::Engine *engine = new kit::Engine();
-	engine->run();
+	::engine = new kit::Engine();
+
+	struct sigaction interruptHandler;
+	sigemptyset(&interruptHandler.sa_mask);
+	interruptHandler.sa_handler = ::handleInterrupt;
+	interruptHandler.sa_flags = 0;
+
+	sigaction(SIGINT, &interruptHandler, NULL);
+
+	::engine->run();
 	
 	return 0;
 }
